1bc3b3d9427b374c808cb1b9bf45391bf364094f
Francisco Garay <voyager-dev@protonmail.com>
2022-12-26 16:27:47 +0100

    Sidebar Structure ready. Starting Discover Component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,9 +5,16 @@ import Link from "next/link";
 import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
+import Discover from "@components/Discover";
+import SuggestedAccounts from "@components/SuggestedAccounts";
+import Footer from "@components/Footer";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState<boolean>(true);
+  const normalLink: string =
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded";
+  const userProfile = false;
+
   return (
     <>
       <div
@@ -20,8 +27,41 @@ const Sidebar = () => {
       {showSidebar && (
         <div className="w-20 xl:w-400 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
           <div className="xl:border-b-2 border-gray-200 xl:pb-4">
-            <Link href={"/"}></Link>
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="text-xl hidden xl:block">For You</span>
+              </div>
+            </Link>
           </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">Log in to like and comment videos</p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  render={(renderProps) => (
+                    <button
+                      className="bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      {" "}
+                      Log in
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy="single_host_origin"
+                />
+              </div>
+            </div>
+          )}
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
         </div>
       )}
     </>
