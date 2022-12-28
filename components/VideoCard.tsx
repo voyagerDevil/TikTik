99bd335ebe117db2866219d36929596d5da67436
franchescocrespo <voyager-dev@protonmail.com>
2022-12-28 20:41:40 +0100

    VideoCard component structure and CMS connection ready. Starting Login in Nav component

---
+++ components/VideoCard.tsx
@@ -0,0 +1,107 @@
+import { useState, useEffect, useRef } from "react";
+import { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import { IVideo } from "@types";
+
+interface IProps {
+  post: IVideo;
+}
+
+const VideoCard: NextPage<IProps> = ({ post }: IProps) => {
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
+
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="w-10 md:w-16 h-10 md:h-16">
+            <Link href={"/"}>
+              <>
+                <Image
+                  src={post.postedBy.image}
+                  alt="Profile Photo"
+                  layout="responsive"
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href={"/"}>
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          className="rounded-3xl"
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          <Link href={"/"}>
+            <video
+              src={post.video.asset.url}
+              ref={videoRef}
+              loop
+              muted={isMuted}
+              className="w-[200px] lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] rounded-2xl cursor-pointer bg-gray-100"
+            ></video>
+          </Link>
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
+              {playing ? (
+                <button onClick={handleVideoPress}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={handleVideoPress}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+
+              {isMuted ? (
+                <button onClick={() => setIsMuted(false)}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={() => setIsMuted(true)}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
-import { NextPage } from "next";
+// import { NextPage } from "next";
+import axios from "axios";
+import { IVideo } from "@types";
+import VideoCard from "@components/VideoCard";
+import NoResults from "@components/NoResults";
 
-const Home: NextPage = () => {
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+interface IProps {
+  videos: IVideo[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: IVideo) => (
+          <VideoCard post={video} key={`home-video-${video._id}`} />
+        ))
+      ) : (
+        <NoResults text="No Videos" />
+      )}
+    </div>
   );
 };
 
